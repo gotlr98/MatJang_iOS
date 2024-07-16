2c43b4cc05498e5431aa5fa4beadacb3f5ffb229
gotlr98 <gotlr98@naver.com>
2024-07-16 16:31:25 +0900

    add apple sing in function

--- MatJang/MatJang/View/LoginView.swift
+++ MatJang/MatJang/View/LoginView.swift
@@ -10,32 +10,104 @@ import SwiftUI
 import KakaoSDKCommon
 import KakaoSDKAuth
 import KakaoSDKUser
+import AuthenticationServices
+import JWTDecode
 
 struct LoginView: View{
+    
+    @State var isAppleSignIn: Bool = false
+    @State var isKakaoSignIn: Bool = false
     var body: some View{
-        VStack {
-            Button {
-                if (UserApi.isKakaoTalkLoginAvailable()) {
-                UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
-                        print(oauthToken)
-                        print(error)
-                    }
-                } else {
-                    UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
-                        print(oauthToken)
-                    print(error)
-                    }
+        NavigationView {
+            VStack {
+                NavigationLink(destination: MainMap(), isActive: $isKakaoSignIn, label:{
+                    Button {
+                        if (UserApi.isKakaoTalkLoginAvailable()) {
+                        UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                            if let error = error{
+                                print(error)
+                            }
+                            else{
+                                isKakaoSignIn = true
+                            }
+                        }
+                    } else {
+                            UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                                if let error = error{
+                                    print(error)
+                                }
+                                else{
+                                    isKakaoSignIn = true
+                                }
+                            }
+                        }
+                    } label : {
+                        Image("kakao_login_medium_narrow")
+                            .resizable()
+                            .frame(width : UIScreen.main.bounds.width * 0.9, height:50)
                 }
-            } label : {
-                Image("kakao_login_medium_narrow")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width : UIScreen.main.bounds.width * 0.9)
+                
+                })
+                NavigationLink(destination: MainMap(), isActive: $isAppleSignIn){
+                    SignInWithAppleButton(onRequest: {request in
+                        request.requestedScopes = [.fullName, .email]
+                    }, onCompletion: {result in
+                        switch result{
+                        case .success(let authResults):
+                            print("Sucess")
+                            switch authResults.credential{
+                            case let appleIdCredential as ASAuthorizationAppleIDCredential:
+                                let UserIdentifier = appleIdCredential.user
+                                let fullName = appleIdCredential.fullName
+                                let name = (fullName?.familyName ?? "") + (fullName?.givenName ?? "")
+                                let email = appleIdCredential.email ?? ""
+                                let IdentityToken = String(data: appleIdCredential.identityToken!, encoding: .utf8)
+                                let AuthorizationCode = String(data: appleIdCredential.authorizationCode!, encoding: .utf8)
+                                
+                                
+                                if(email == ""){
+                                    if let identityTokenData = appleIdCredential.identityToken,
+                                       let identityTokenString = String(data: identityTokenData, encoding: .utf8) {
+                                        do {
+                                            let jwt = try decode(jwt: identityTokenString)
+                                            let decodedBody = jwt.body as Dictionary<String, Any>
+                                            print("Decoded email: "+(decodedBody["email"] as? String ?? "n/a")   )
+                                            isAppleSignIn = true
+                                        } catch {
+                                            print("decoding failed")
+                                        }}
+                                }
+                                else{
+                                    print("email:" + email)
+                                    isAppleSignIn = true
+                                }
+                                
+                                
+                                
+                            default:
+                                break
+                            }
+                        case .failure(let error):
+                            print(error.localizedDescription)
+                        }
+                    })
+                    .frame(width : UIScreen.main.bounds.width * 0.9, height:50)
+                    .cornerRadius(5)
+                }
+                
+                
             }
+            .padding()
         }
-        .padding()
     }
 }
+
+//struct SignInWithApple: View{
+//    @State var isActive: Bool = false
+//    var body: some View{
+//        
+//    }
+//}
 
 #Preview{
     LoginView()
---
+++ MatJang/MatJang/View/MainMap.swift
@@ -0,0 +1,18 @@
+//
+//  MainMap.swift
+//  MatJang
+//
+//  Created by HaeSik Jang on 7/16/24.
+//
+
+import SwiftUI
+
+struct MainMap: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainMap()
+}
