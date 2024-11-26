c908a4d8a45b3f064dbcc6e1b0bc08ee440300da
gotlr98 <gotlr98@naver.com>
2024-11-26 11:34:19 +0900

    write bookmark at firestore

--- MatJang_iOS/MatJang_iOS/View/AddBookmarkView.swift
+++ MatJang_iOS/MatJang_iOS/View/AddBookmarkView.swift
@@ -9,9 +9,13 @@ import Foundation
 import UIKit
 import Then
 import SnapKit
+import FirebaseFirestore
 
 class AddBookmarkView: UIViewController{
     
+    var addBookmarkName: [String] = []
+    
+    let db = Firestore.firestore()
     
     private lazy var addButton = UIButton().then{
         $0.setTitle("추가하기", for: .normal)
@@ -19,6 +23,8 @@ class AddBookmarkView: UIViewController{
         $0.setImage(UIImage(systemName: "plus.app"), for: .normal)
         $0.addTarget(self, action: #selector(onTouchAddButton), for: .touchUpInside)
     }
+    
+    private lazy var bookmarkListButton: [UIButton] = []
     
     @objc func onTouchAddButton(){
         let bookmarkAddAlert = UIAlertController(title: "즐겨찾기명을 입력해주세요", message: "", preferredStyle: .alert)
@@ -29,8 +35,21 @@ class AddBookmarkView: UIViewController{
         }
         
         let okAction = UIAlertAction(title: "등록", style: .default){ action in
-            guard let textField = bookmarkAddAlert.textFields?.first,
-                    let text = textField.text else{
+            let textField = bookmarkAddAlert.textFields?.first
+            let text = textField?.text ?? ""
+            
+            self.addBookmarkName.append(text)
+            self.view.layoutIfNeeded()
+            
+            let user_email = UserDefaults.standard.string(forKey: "isAutoLogin")
+            
+            self.db.collection("users").document(user_email ?? "").updateData(["bookmark":[text:[]]])
+            
+            print(text)
+            
+            self.viewDidLoad()
+            
+            if(text == ""){
                 self.showToast(message: "메시지를 입력해주세요")
                 return
             }
@@ -46,7 +65,7 @@ class AddBookmarkView: UIViewController{
     
     func showToast(message : String, font: UIFont = UIFont.systemFont(ofSize: 14.0)){
 
-            let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+            let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-10, width: 150, height: 35))
             toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
             toastLabel.textColor = UIColor.white
             toastLabel.font = font
@@ -72,5 +91,21 @@ class AddBookmarkView: UIViewController{
             make.top.equalTo(self.view.snp.top).offset(20)
             make.centerX.equalTo(self.view.snp.centerX)
         })
+        
+        var count = 0
+        
+        if(!self.addBookmarkName.isEmpty){
+            bookmarkListButton.append(UIButton(frame: CGRect(x: 10, y: 10, width: 50, height: 50)))
+            for te in bookmarkListButton{
+                self.view.addSubview(te)
+                te.setTitle(addBookmarkName[count], for: .normal)
+                te.snp.makeConstraints({ make in
+                    make.top.equalTo(self.addButton.snp.bottom).offset(10)
+                })
+                count += 1
+            }
+            
+            self.view.layoutIfNeeded()
+        }
     }
 }
